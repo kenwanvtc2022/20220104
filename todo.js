28fc63adeb31614cf04f98566259e3fecf536c2d
kenwanvtc2022 <97097842+kenwanvtc2022@users.noreply.github.com>
2022-01-04 13:01:59 +0000

    workshop1 todo.js

--- todo.js
+++ todo.js
@@ -6,19 +6,35 @@
 
 const readline = require('readline-sync')
 
-var items = []
+const items = []
+const input = '' //input value must be defined as exit or user cannot exit the do-while block
 
 do {
-  var input = String(readline.question('enter command: ')).trim()
+  const input = String(readline.question('enter command: ')).trim() //Failure to run with error "input is not defined" after change from var to let
+  //let can only run inside the block, which is do-while block in this situation
   if (input.indexOf('add ') === 0) {
     const space = input.indexOf(' ')
     const item = input.substring(space).trim()
     console.log(`adding "${item}"`)
-    items.push(item)
+    if (items.indexOf(item.toLowerCase()) < 0) {
+      items.push(item)
+    } else {
+      console.log(`Item Added. Please enter again.`)
+    }
+  }
+  if (input.indexOf('remove ') === 0) {
+    const space = input.indexOf(' ')
+    const item = input.substring(space).trim()
+    console.log(`removing "${item}"`)
+    const pos = items.indexOf(item.toLowerCase())
+    items.splice(pos, 1)
   }
   if (input.indexOf('list') === 0) {
     for (let i = 0; i < items.length; i++) {
       console.log(`${i}. ${items[i]}`)
     }
+  }
+  if (input.indexOf('exit') === 0) {
+    break
   }
 } while (input !== 'exit')
